9fb8b94d5268591499efbd2af3b9aa493b4dcbd8
Happy-negi238 <happynegi238@gmail.com>
2026-02-16 19:02:38 +0530

    dashboard home page complete

--- src/components/adminHome/AdminHome.tsx
+++ src/components/adminHome/AdminHome.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+'use client'
 import Image from "next/image";
 import logo from "../../../public/assets/img/LOgo-PNG.png";
 import Link from "next/link";
 import product2 from "../../../public/assets/img/thumb/Swati Ray.png";
+import { useState } from "react";
 
 const AdminHome = () => {
   return (
@@ -135,6 +136,132 @@ const AdminHome = () => {
                   </div>
                 </div>
               </div>
+              <div className="dashboardTestmonial-clients">
+                <div className="dashboard-testmonial">
+                    <div className="dashboard-testmonial-container">
+                      <h3>Client's review</h3>
+
+                      <div className="">
+                        <Image
+                          src={product2}
+                          alt="admin-image"
+                          className="client-image"
+                        />
+                        
+                          <ClientReview text="My experience with you has been very good and I am very happy with your work.
+                          You have passion and patience.It would be a great pleasure to work with you in the 
+                          future and to share your work with everyone. It was a great satisfaction for me. 
+                          Thank you, as I do. We will work together in the future. 
+                          Best wishes for the future.Thank you. Chakme Transolution PVT LTD."/>
+                      </div>
+                      <div className="dashboard-divide"></div>
+                      <div className="">
+                        <Image
+                          src={product2}
+                          alt="admin-image"
+                          className="client-image"
+                        />
+                        
+                          <ClientReview text="My experience with you has been very good and I am very happy with your work.
+                          You have passion and patience.It would be a great pleasure to work with you in the 
+                          future and to share your work with everyone. It was a great satisfaction for me. 
+                          Thank you, as I do. We will work together in the future. 
+                          Best wishes for the future.Thank you. Chakme Transolution PVT LTD."/>
+                      </div>
+                      <div className="dashboard-divide"></div>
+
+                      <div className="">
+                        <Image
+                          src={product2}
+                          alt="admin-image"
+                          className="client-image"
+                        />
+                        
+                          <ClientReview text="My experience with you has been very good and I am very happy with your work.
+                          You have passion and patience.It would be a great pleasure to work with you in the 
+                          future and to share your work with everyone. It was a great satisfaction for me. 
+                          Thank you, as I do. We will work together in the future. 
+                          Best wishes for the future.Thank you. Chakme Transolution PVT LTD."/>
+                      </div>
+                      <div className="dashboard-divide"></div>
+
+                      <div className="">
+                        <Image
+                          src={product2}
+                          alt="admin-image"
+                          className="client-image"
+                        />
+                        
+                          <ClientReview text="My experience with you has been very good and I am very happy with your work.
+                          You have passion and patience.It would be a great pleasure to work with you in the 
+                          future and to share your work with everyone. It was a great satisfaction for me. 
+                          Thank you, as I do. We will work together in the future. 
+                          Best wishes for the future.Thank you. Chakme Transolution PVT LTD."/>
+                      </div>
+                      <div className="dashboard-divide"></div>
+
+                      <div className="">
+                        <Image
+                          src={product2}
+                          alt="admin-image"
+                          className="client-image"
+                        />
+                        
+                          <ClientReview text="My experience with you has been very good and I am very happy with your work.
+                          You have passion and patience.It would be a great pleasure to work with you in the 
+                          future and to share your work with everyone. It was a great satisfaction for me. 
+                          Thank you, as I do. We will work together in the future. 
+                          Best wishes for the future.Thank you. Chakme Transolution PVT LTD."/>
+                      </div>
+                      <div className="dashboard-divide"></div>
+                      
+                    </div>
+                </div>
+                <div className="dashboard-clients">
+                  <div className="client-testmonial-container">
+                    <h3>Users</h3>
+                    <div className="dashboard-user-detail">
+                      <div className="">
+                        <h4>Faizan</h4>
+                        <a href="">faizansheikh@gmail.com</a>
+                        <a href="">9099568908</a>
+                      </div>
+                      <p>I want to design webste</p>
+                    </div>
+                    <div className="dashboard-divide"></div>
+
+                    <div className="dashboard-user-detail">
+                      <div className="">
+                        <h4>Faizan</h4>
+                        <a href="">faizansheikh@gmail.com</a>
+                        <a href="">9099568908</a>
+                      </div>
+                      <p>I want to design webste</p>
+                    </div>
+                    <div className="dashboard-divide"></div>
+
+                    <div className="dashboard-user-detail">
+                      <div className="">
+                        <h4>Faizan</h4>
+                        <a href="">faizansheikh@gmail.com</a>
+                        <a href="">9099568908</a>
+                      </div>
+                      <p>I want to design webste</p>
+                    </div>
+                    <div className="dashboard-divide"></div>
+
+                    <div className="dashboard-user-detail">
+                      <div className="">
+                        <h4>Faizan</h4>
+                        <a href="">faizansheikh@gmail.com</a>
+                        <a href="">9099568908</a>
+                      </div>
+                      <p>I want to design webste</p>
+                    </div>
+                    <div className="dashboard-divide"></div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -143,4 +270,37 @@ const AdminHome = () => {
   );
 };
 
+type ReadmoreProps = {
+  text: string;
+  limit?: number;
+};
+
+const ClientReview = ({ text, limit = 50 }: ReadmoreProps) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const words = text.split(" ");
+  const showButton = words.length > limit;
+
+  const displayText = expanded
+    ? text
+    : words.slice(0, limit).join(" ") + "...";
+
+  return (
+    <div>
+      <p className="dashboard-review-para">{displayText}</p>
+
+      {showButton && (
+        <span
+          onClick={() => setExpanded(!expanded)}
+          style={{
+            cursor: "pointer",
+            fontSize: "14px",
+          }}
+        >
+          {expanded ? "Read less" : "Read more"}
+        </span>
+      )}
+    </div>
+  );
+};
 export default AdminHome;
